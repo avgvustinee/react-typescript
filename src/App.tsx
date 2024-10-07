64028d1d3c82b7539a358002821b7adc23fd8c0a
codewithsyre <augustineshoks7@gmail.com>
2024-10-07 17:43:24 +0200

    statehook commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
-import Like from "./components/Like";
+import { useState } from "react";
+import Button from "./components/Button/Button";
 
 const App = () => {
+  const [isVisible, setAlertVisibility] = useState(false);
+  const handleClick = ()=>{
+    setAlertVisibility(true);
+    console.log(isVisible);
+  }
   return (
     <div>
-      <Like onClick={()=> console.log('clicked')} />
+    <Button onClick={handleClick}>
+      Show
+    </Button>
     </div>
   );
 };
