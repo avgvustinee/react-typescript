4e37eeb23b9f12f0780ddb85c6d7e4f8f4239d10
codewithsyre <augustineshoks7@gmail.com>
2024-10-08 18:42:05 +0200

    SharingStatesComponent commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import { useState } from "react";
-import Button from "./components/Button/Button";
-import {produce} from 'immer'
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 const App = () => {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-    { id: 3, title: "Bug 3", fixed: false },
-  ]);
-  const handleClick = () => {
-    // update
-    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-
-    // using immer function
-    setBugs(produce(draft => {
-      const bug = draft.find(bug=> bug.id === 1);
-      if(bug) bug.fixed = true;
-      
-      }))
-   
-
-  };
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
   return (
     <div>
-    {bugs.map(bug=> <p key={bug.id}> {bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
-      <Button onClick={handleClick}>Show</Button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 };
