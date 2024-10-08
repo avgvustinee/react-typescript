aeaa5c7448145a0ce899c74ecd9745f27a5c1190
codewithsyre <augustineshoks7@gmail.com>
2024-10-08 23:22:19 +0200

    HandlingFormSubmission commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-        molestias accusantium modi delectus quas, adipisci ullam velit, sed
-        vitae aspernatur dolores consectetur in! Repellendus similique ea at
-        quasi! Quidem saepe, accusantium temporibus beatae blanditiis ipsa
-        officia pariatur excepturi voluptates eius, dolor cum rerum repudiandae
-        libero! Nulla, id! Sunt ad quia vitae odit dolores, libero quasi sequi
-        velit autem. Repellendus rem, consectetur beatae ipsum ut error fugiat
-        provident eligendi ex sint recusandae reprehenderit, dolores tempore
-        exercitationem dolorum earum sequi alias excepturi voluptatibus
-        doloremque. Facilis, exercitationem consequuntur magni accusantium nemo
-        aliquid rem praesentium? Ut repudiandae mollitia eligendi labore
-        delectus numquam. Atque, similique!
-      </ExpandableText>
+      <Form />
     </div>
   );
 };
