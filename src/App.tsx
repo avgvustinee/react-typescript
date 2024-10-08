d561059951d40127cde128fb75220ba3c238d57b
codewithsyre <augustineshoks7@gmail.com>
2024-10-08 19:03:19 +0200

    ExpandableText commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+
+import ExpandableText from "./components/ExpandableText";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
+        molestias accusantium modi delectus quas, adipisci ullam velit, sed
+        vitae aspernatur dolores consectetur in! Repellendus similique ea at
+        quasi! Quidem saepe, accusantium temporibus beatae blanditiis ipsa
+        officia pariatur excepturi voluptates eius, dolor cum rerum repudiandae
+        libero! Nulla, id! Sunt ad quia vitae odit dolores, libero quasi sequi
+        velit autem. Repellendus rem, consectetur beatae ipsum ut error fugiat
+        provident eligendi ex sint recusandae reprehenderit, dolores tempore
+        exercitationem dolorum earum sequi alias excepturi voluptatibus
+        doloremque. Facilis, exercitationem consequuntur magni accusantium nemo
+        aliquid rem praesentium? Ut repudiandae mollitia eligendi labore
+        delectus numquam. Atque, similique!
+      </ExpandableText>
     </div>
   );
 };
