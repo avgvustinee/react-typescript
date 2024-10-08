175dbe4cab391615c453f63ff12acc82db6de8fd
codewithsyre <augustineshoks7@gmail.com>
2024-10-08 13:13:55 +0200

    SimplifyingLogicWithImmer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./components/Button/Button";
+import {produce} from 'immer'
 
 const App = () => {
   const [bugs, setBugs] = useState([
@@ -9,12 +10,21 @@ const App = () => {
   ]);
   const handleClick = () => {
     // update
-    setBugs(bugs.map(bug=> bug.id === 1 ? {...bug, fixed:true} : bug));
-  };
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
 
+    // using immer function
+    setBugs(produce(draft => {
+      const bug = draft.find(bug=> bug.id === 1);
+      if(bug) bug.fixed = true;
+      
+      }))
+   
+
+  };
 
   return (
     <div>
+    {bugs.map(bug=> <p key={bug.id}> {bug.title} {bug.fixed ? 'Fixed' : 'New'}</p>)}
       <Button onClick={handleClick}>Show</Button>
     </div>
   );
