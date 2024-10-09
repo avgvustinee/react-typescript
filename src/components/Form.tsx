00d5c7b85a4909561ad1cad5ab1762491d18a882
codewithsyre <augustineshoks7@gmail.com>
2024-10-09 11:16:47 +0200

    ApplyingValidation commit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,37 +1,39 @@
-import { FormEvent, useRef } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData{
+  name:string;
+  age:number;
+}
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "",age: 0,};
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name= nameRef.current.value;
-    if (ageRef.current !== null) person.age =parseInt(ageRef.current.value);
-    console.log(person);
-  };
+  const { register, handleSubmit,formState: {errors} } = useForm<FormData>();
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          ref={nameRef}
+          {...register("name", {required:true, minLength:3})}
           id="name"
           type="text"
           className="form-control"
           style={{ width: "300px" }}
         />
+        { errors.name?.type ==='required' && <p className="text-danger">The name field is required</p>}
+        {errors.name?.type ==='minLength' && <p className="text-danger">The name must be at least 3 characters</p>} 
+
+
+
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          ref={ageRef}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
